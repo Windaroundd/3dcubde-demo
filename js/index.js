afc3f2f8bb9835b90167a2435b3c9f3021a5cb00
Phong Huynh <phonghqm@gmail.com>
2023-05-23 16:35:18 +0700

    initial commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  //direction: 'vertical',
+  loop: true,
+  effect: "cube",
+  cubeEffect: {
+    slideShadows: false,
+    shadow: false,
+  },
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
